3afa29ad57a2188f0eb22a7aa84483f4ef2a48db
niam-foxel <nils.hamel@foxel.ch>
2014-09-15 13:41:13 +0200

    BAIFA renaming to IOBMA

---
+++ src/csps-imu-mod-IOBMA.h
@@ -0,0 +1,120 @@
+/*
+ * libcsps - Crossed-signal positioning system library
+ *
+ * Copyright (c) 2013-2014 FOXEL SA - http://foxel.ch
+ * Please read <http://foxel.ch/license> for more information.
+ *
+ *
+ * Author(s):
+ *
+ *      Nils Hamel <n.hamel@foxel.ch>
+ *
+ *
+ * This file is part of the FOXEL project <http://foxel.ch>.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * Additional Terms:
+ *
+ *      You are required to preserve legal notices and author attributions in
+ *      that material or in the Appropriate Legal Notices displayed by works
+ *      containing it.
+ *
+ *      You are required to attribute the work as explained in the "Usage and
+ *      Attribution" section of <http://foxel.ch/license>.
+ */
+
+    /*! \file   csps-imu-mod-IOISA.h
+     *  \author Nils Hamel (n.hamel@foxel.ch)
+     *
+     *  IMU earth self-alignment module (IOISA - Initial Orientation Inertial Self-Alignment)
+     */
+
+/*
+    Header - Include guard
+ */
+
+    # ifndef __LP_IMU_BAIFA__
+    # define __LP_IMU_BAIFA__
+
+/*
+    Header - C/C++ compatibility
+ */
+
+    # ifdef __cplusplus
+    extern "C" {
+    # endif
+
+/*
+    Header - Includes
+ */
+
+    # include "csps.h"
+    # include "csps-device.h"
+    # include "csps-path.h"
+    # include "csps-stream.h"
+    # include "csps-timestamp.h"
+
+/*
+    Header - Preprocessor definitions
+ */
+
+    /* CSPS module identification */
+    # define LP_IMU_IOBMA_MOD "mod-IOBMA"
+    # define LP_IMU_IOBMA_DES "Initial Orientation Body Momentum Alignment"
+
+/*
+    Header - Preprocessor macros
+ */
+
+/*
+    Header - Typedefs
+ */
+
+/*
+    Header - Structures
+ */
+
+/*
+    Header - Function prototypes
+ */
+
+    /*! \brief Initial orientation body momentum alignment module
+     *  
+     */
+
+    lp_Void_t lp_imu_mod_IOBMA(
+
+        const lp_Char_t * const lpPath, 
+        lp_IMU                  lpIMU,
+        lp_GPS                  lpGPS,
+        const lp_Char_t * const lpIMUmodGEO  
+
+    );
+
+/*
+    Header - C/C++ compatibility
+ */
+
+    # ifdef __cplusplus
+    }
+    # endif
+
+/*
+    Header - Include guard
+ */
+
+    # endif
+
